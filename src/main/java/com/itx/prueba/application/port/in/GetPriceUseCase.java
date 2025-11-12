3c62354bfd780aceb374de150150b46b038bd319
rbejerano-84 <rbejerano@gmail.com>
2025-11-12 15:32:29 +0100

    Delete src directory

--- src/main/java/com/itx/prueba/application/port/in/GetPriceUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.itx.prueba.application.port.in;
-
-import com.itx.prueba.domain.model.Price;
-import com.itx.prueba.domain.model.PriceRangeQuery;
-
-public interface GetPriceUseCase {
-    Price getPriceByIdIdCadDate(PriceRangeQuery request);
-}
--- src/main/java/com/itx/prueba/infrastructure/adapter/in/restapi/exception/PriceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.itx.prueba.infrastructure.adapter.in.restapi.exception;
-
-public class PriceNotFoundException extends RuntimeException {
-    public PriceNotFoundException(String message) {
-        super(message);
-    }
-}
